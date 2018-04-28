c5bb47970677191ddda8ce37c97f7df97d120e9b
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2018-04-28 11:17:33 -0400

    Adding configurable messages + removing color from onEnable message bc of poggit -_-

--- src/iiFlamiinBlaze/BlazinFly/BlazinFly.php
+++ src/iiFlamiinBlaze/BlazinFly/BlazinFly.php
@@ -33,30 +33,30 @@ use pocketmine\utils\TextFormat;
 
 class BlazinFly extends PluginBase implements Listener{
 
-    const PREFIX = TextFormat::GREEN . "BlazinFly" . TextFormat::AQUA . " > " . TextFormat::WHITE;
+    const PREFIX = TextFormat::AQUA . "BlazinFly" . TextFormat::GOLD . " > ";
+    const VERSION = "v1.8.3";
 
     public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
-        $this->getLogger()->info(self::PREFIX . "BlazinFly by iiFlamiinBlaze enabled");
+        $this->getLogger()->info("BlazinFly " . self::VERSION . " by iiFlamiinBlaze enabled");
     }
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
-        $config = $this->getConfig();
-        if($config->getNested("onJoin_FlyReset") === true){
+        if($this->getConfig()->get("onJoin_FlyReset") === true){
             if($player->isCreative()) return;
             $player->setAllowFlight(false);
             $player->setFlying(false);
-            $player->sendMessage(self::PREFIX . TextFormat::RED . "Flight has been disabled");
+            $player->sendMessage($this->getConfig()->get("fly_disabled"));
         }
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
         if($command->getName() === "fly"){
             if(!$sender instanceof Player){
-                $sender->sendMessage(TextFormat::RED . "Use this command in-game");
+                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Use this command in-game");
                 return false;
             }
             if(!$sender->hasPermission("fly.command")){
@@ -67,11 +67,11 @@ class BlazinFly extends PluginBase implements Listener{
                 if(!$sender->getAllowFlight()){
                     $sender->setAllowFlight(true);
                     $sender->setFlying(true);
-                    $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "Flight mode activated");
+                    $sender->sendMessage($this->getConfig()->get("fly_enabled"));
                 }else{
                     $sender->setAllowFlight(false);
                     $sender->setFlying(false);
-                    $sender->sendMessage(self::PREFIX . TextFormat::RED . "Regular mode activated");
+                    $sender->sendMessage($this->getConfig()->get("fly_disabled"));
                 }
             }else{
                 $sender->sendMessage(self::PREFIX . TextFormat::RED . "You can only use this command in survival mode");
@@ -83,8 +83,7 @@ class BlazinFly extends PluginBase implements Listener{
 
     public function onDamage(EntityDamageEvent $event) : void{
         $entity = $event->getEntity();
-        $config = $this->getConfig();
-        if($config->getNested("onDamage_FlyReset") === true){
+        if($this->getConfig()->get("onDamage_FlyReset") === true){
             if($event instanceof EntityDamageByEntityEvent){
                 if($entity instanceof Player){
                     $damager = $event->getDamager();
